0c814c59ec19d941238a911979c4b994b5a64022
AnnaSosik <125738831+AnnaSosik@users.noreply.github.com>
2023-10-14 19:27:23 +0200

    Main update

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import { List } from './FriendList.styled';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+
+export default function FriendList({ friends }) {
+    return (
+      <List className="friend-list">
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        ))}
+      </List>
+    );
+  }
+  
+  FriendList.propTypes = {
+    friends: PropTypes.array.isRequired,
+  };
